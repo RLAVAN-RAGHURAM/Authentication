ec0c2a3b51a9ec93499cbe0de84d1e562dc0f31c
Lavan <r.lavan.mail@gmail.com>
2022-09-28 18:57:07 +0530

    first commit

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ def verify_otp():
     mobile_number = request.form['number']
 
     if username == 'verify' and password == '12345':   
-        account_sid = 'ACc07131165619ddab29e35e673d392f32'
-        auth_token = 'IS174a159f9d11e547e003d859d8baaa70'
+        account_sid = 'ACf46479ad928255bf9f54f3da69cc1d0d'
+        auth_token = '3dd731539b4c73acb8ad98743af70903'
         client = Client(account_sid, auth_token)
 
         verification = client.verify \
-            .services('IS174a159f9d11e547e003d859d8baaa70') \
+            .services('VAa1b9665035fd8b1f783e195efe24b679') \
             .verifications \
             .create(to=mobile_number, channel='sms')
 
@@ -43,12 +43,12 @@ def get_otp():
     received_otp = request.form['received_otp']
     mobile_number = request.form['number']
 
-    account_sid = 'ACc07131165619ddab29e35e673d392f32'
-    auth_token = 'IS174a159f9d11e547e003d859d8baaa70'
+    account_sid = 'ACf46479ad928255bf9f54f3da69cc1d0d'
+    auth_token = '3dd731539b4c73acb8ad98743af70903'
     client = Client(account_sid, auth_token)
                                             
     verification_check = client.verify \
-        .services('IS174a159f9d11e547e003d859d8baaa70') \
+        .services('VAa1b9665035fd8b1f783e195efe24b679') \
         .verification_checks \
         .create(to=mobile_number, code=received_otp)
     print(verification_check.status)
